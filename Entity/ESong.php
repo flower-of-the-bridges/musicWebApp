5d83a4eb899803fddbdf9caf1e7712ddaf052a7d
FlowerOfTheBridges <gfiordeponti@gmail.com>
2018-04-19 11:14:09 +0200

    Added comments to ESong

--- Entity/ESong.php
+++ Entity/ESong.php
@@ -33,22 +33,40 @@ class ESong
 
     private $registeredOnly;
 
+
+
     /**
      * Inizializza una canzone. La visibilita' e' di default solo
      * per gli utenti registrati
      * @param string $name il nome del brano
+     * @param string $artist il nome dell'artista
      * @param string $genre il genere del brano
      */
-    public function __construct(string $name, string $genre)
+    public function __construct(string $name, string $artist,string $genre)
     {
         $this->name = $name;
+        $this->artist=$artist;
         $this->genre = $genre;
         $this->forAll = false;
         $this->supportersOnly = false;
         $this->registeredOnly = true;
     }
 
+    /**
+     * @return string
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
     
+    /**
+     * @param string $artist
+     */
+    public function setArtist($artist)
+    {
+        $this->artist = $artist;
+    }
     function getText(): string
     {
         return $this->lyrics;
@@ -151,7 +169,7 @@ class ESong
    /**
     * Imposta la visibilita' per tutti gli utenti.
     */
-    function setForAll()
+    function setForAll() : void
     {
         $this->All = true;
         $this->supportersOnly = true;
@@ -161,7 +179,7 @@ class ESong
     /**
      * Imposta la visibilita' solo per chi supporta l'artista
      */
-    function setForSupportersOnly()
+    function setForSupportersOnly() : void
     {
         $this->All = false;
         $this->registeredOnly = false;
@@ -173,7 +191,7 @@ class ESong
     /**
      * Imposta la visibilita' solo per chi e' registrato
      */
-    function setForRegisteredOnly()
+    function setForRegisteredOnly() : void
     {
         $this->All = false;
         $this->supportersOnly = true;
@@ -183,13 +201,18 @@ class ESong
     /**
      * Nasconde il brano a tutti gli utenti
      */
-    function hide()
+    function hide() : void
     {
         $this->All = false;
         $this->supportersOnly = false;
         $this->registeredOnly = false;
     }
 
+    function __toString(){
+        $string="Nome :".$this->name."\nArtista: ".$this->artist."\nGenere: ".$this->genre;
+        return $string;
+        
+    }
 
     
 }
---
+++ debug.php
@@ -0,0 +1,6 @@
+<?php
+require 'Entity/ESong.php';
+$s=new ESong("2112", "Rush","Rock");
+
+echo($s);
+?>
