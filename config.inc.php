56f3c54925139b1468ddd5f3c6305c6e4661ebed
Lautz97 <davide.lauterio@gmail.com>
2018-04-26 10:16:01 +0200

    added addressing for remote server in config.inc

--- config.inc.php
+++ config.inc.php
@@ -1,8 +1,16 @@
 <?php
+/*
 $address= 'localhost';
 $database= 'musicwebapp';
 $user= 'root';
 $pass= 'web';
+*/
+#/*
+$address= 'localhost';
+$database= 'id5519936_gruppo2';
+$user= 'id5519936_root';
+$pass= 'gruppo2cicerone';
+#*/
 ?>
 
 
