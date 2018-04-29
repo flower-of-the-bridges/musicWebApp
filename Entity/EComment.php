a1ad324c04d932f30652be15fa08b4324c3b3ecc
FlowerOfTheBridges <gfiordeponti@gmail.com>
2018-04-29 12:10:06 +0200

    added basic operation for comments

--- Entity/EComment.php
+++ Entity/EComment.php
@@ -3,5 +3,67 @@
  * @author gruppo 2
  */
 class EComment {
-    //put your code here
+    
+    private $songId;
+    
+    private $user;
+    
+    private $comment;
+    
+    function __construct(int $songId, string $user, string $comment){
+        $this->songId=$songId;
+        $this->user=$user;
+        $this->comment=$comment;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getSongId() : int
+    {
+        return $this->songId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser() : string
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComment() : string
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @param mixed $songId
+     */
+    public function setSongId(int $songId)
+    {
+        $this->songId = $songId;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser(string $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @param mixed $comment
+     */
+    public function setComment(string $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    
+    
 }
