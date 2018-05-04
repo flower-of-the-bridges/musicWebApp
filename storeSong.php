f8f952788d4398b373ed1b71bff0225b8a165952
LautzFromAnotherPlace <davide.lauterio@gmail.com>
2018-05-04 15:54:54 +0200

    fixed song constructors in storeSong.php null id

--- storeSong.php
+++ storeSong.php
@@ -5,20 +5,20 @@ require 'inc.php';
 $p=FPersistantManager::getInstance(); //creo istanza del manager di connessione al dbms
 
 //prima insert
-$s=new ESong("2112", "Rush","Rock");
+$s=new ESong(null, "2112", "Rush","Rock");
 $s->setFilePath('./prova.mp3');
 $p->store($s);
 echo($s);
 
 //seconda insert
-$s=new ESong("Tom Sawyer", "Rush","Rock");
+$s=new ESong(null, "Tom Sawyer", "Rush","Rock");
 $s->setForSupportersOnly();
 $s->setFilePath('./prova.mp3');
 $p->store($s);
 echo($s);
 
 //terza insert
-$s=new ESong("YXZ", "Rush","Rock");
+$s=new ESong(null, "YXZ", "Rush","Rock");
 $s->setForSupportersOnly();
 $s->setFilePath('./prova.mp3');
 $p->store($s);
