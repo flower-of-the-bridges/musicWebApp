ed1f9089519872d8682bd04830200ad85a7c922b
Lautz97 <davide.lauterio@gmail.com>
2018-05-07 22:37:54 +0200

    debug method fix

--- storeSong.php
+++ storeSong.php
@@ -4,19 +4,21 @@ require 'inc.php';
 
 $p = FPersistantManager::getInstance(); //creo istanza del manager di connessione al dbms
 
+$mus = new EMusician(1, Rush, null, null, Rock);
+
 //prima insert
-$s = new ESong(null, "2112", "Rush","Rock");
+$s = new ESong(null, "2112", $mus,"Rock");
 $s->setFilePath('./prova.mp3');
 if ($p->store($s)) { echo($s); }
 
 //seconda insert
-$s=new ESong(null, "Tom Sawyer", "Rush","Rock");
+$s=new ESong(null, "Tom Sawyer", $mus,"Rock");
 $s->setForSupportersOnly();
 $s->setFilePath('./prova.mp3');
 if ($p->store($s)) { echo($s); }
 
 //terza insert
-$s=new ESong(null, "YXZ", "Rush","Rock");
+$s=new ESong(null, "YXZ", $mus,"Rock");
 $s->setForSupportersOnly();
 $s->setFilePath('./prova.mp3');
 if ($p->store($s)) { echo($s); }
