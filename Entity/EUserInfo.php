73d38b92046bad6eb57b0f237c08905cc17017f2
Lautz97 <davide.lauterio@gmail.com>
2018-05-22 13:14:40 +0200

    modified EUserInfo (generateGenere) but this have to be fixed, completed FUserInfo, missing user info table, must create EImg and FImg

--- Entity/EUserInfo.php
+++ Entity/EUserInfo.php
@@ -10,13 +10,37 @@ class EUserInfo extends EObject
     private $birthPlace;
     private $birthDate;
     private $bio;
-    private $imgID;
     private $genre;
     
     function __construct()
     {
         parent::__construct();
     }
+    
+    ///////PLS GIO FIX THIS
+    function generateGenre(string $genre = null)
+    {
+        if($genre!=NULL)
+        {
+            $this->genre = $genre;
+        }
+        else
+        {//////////what to do here???????
+            $this->genre = ''; //inizializza il genere
+            $songs = FPersistantManager::getInstance()->load('musicianSongs', $this->id);
+            if($songs)
+            {
+                foreach ($songs as $song)
+                {
+                    $songGenre = $song->getGenre();
+                    if(!preg_match('/\b'.$songGenre.'\b/',$this->genre)) //verifica che il genere non sia gia stato inserito
+                        $this->genre.=$songGenre." "; // aggiunge il valore al genere
+                }
+            }
+            FPersistantManager::getInstance()->update($this);
+        }
+    }
+    
     
     function setFirstName (string $firstName)
     {
@@ -61,15 +85,6 @@ class EUserInfo extends EObject
     function getBio () : string
     {
         return $this->bio;
-    }
-    
-    function setImgId (int $imgID)
-    {
-        $this->imgID = $imgID;
-    }
-    function getImgId () : int
-    {
-        return $this->imgID;
     }
     
     function setGenre (string $genre)
--- Foundation/FUserInfo.php
+++ Foundation/FUserInfo.php
@@ -5,6 +5,32 @@
 class FUserInfo
 {
     
+    static function storeUserInfo() : string
+    {
+        return "INSERT INTO user_info(id, first_name, last_name, birth_place, birth_date, bio, genre)
+                VALUES (:id, :first_name, :last_name, :birth_place, :birth_date, :bio, :genre);";
+    }
+    
+    static function updateUserInfo() : string
+    {
+        return "UPDATE user_info
+                SET id = :id, 
+                    first_name = :first_name,
+                    last_name = :last_name,
+                    birth_place = :birth_place, 
+                    birth_date = :birth_date, 
+                    bio = :bio,   
+                    genre = :genre
+                WHERE id = :id;";
+    }
+    
+    static function loadUserInfo() : string
+    {
+        return "SELECT *
+                FROM user_info
+                WHERE id = :id;";
+    }
+    
     static function searchUserByGenre() : string
     {
         return "SELECT id
@@ -14,9 +40,43 @@ class FUserInfo
     
     static function bindValues(PDOStatement &$stmt, EUserInfo &$userInfo)
     {
-        if($userInfo->getGenre()!=NULL)
+        if($userInfo->getId()!=NULL){
+            $stmt->bindValue(':id', $userInfo->getId(), PDO::PARAM_STR);
+        }
+        if($userInfo->getFirstName()!=NULL){
+            $stmt->bindValue(':first_name', $userInfo->getFirstName(), PDO::PARAM_STR);
+        }    
+        if($userInfo->getLastName()!=NULL){
+            $stmt->bindValue(':last_name', $userInfo->getLastName(), PDO::PARAM_STR);
+        }
+        if($userInfo->getBirthPlace()!=NULL){
+            $stmt->bindValue(':birth_place', $userInfo->getBirthPlace(), PDO::PARAM_STR);
+        }
+        if($userInfo->getBirthDate()!=NULL){
+            $stmt->bindValue(':birth_date', $userInfo->getBirthDate(), PDO::PARAM_STR);
+        }
+        if($userInfo->getBio()!=NULL){
+            $stmt->bindValue(':bio', $userInfo->getBio(), PDO::PARAM_STR);
+        }
+        if($userInfo->getGenre()!=NULL){
             $stmt->bindValue(':genre', $userInfo->getGenre(), PDO::PARAM_STR);
+        }
             
+    }
+    
+    static function createObjectFromRow($row) : EUserInfo
+    {
+        $user = new EUserInfo();
+        
+        $user->setId($row['id']);
+        $user->setFirstName($row['first_name']);
+        $user->setLastName($row['last_name']);
+        $user->setBirthPlace($row['birth_place']);
+        $user->setBirthDate($row['birth_date']);
+        $user->setBio($row['bio']);
+        $user->setGenre($row['genre']);
+        
+        return $user;
     }
     
 }
