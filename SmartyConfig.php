f06274c63a5c52ae15ead4c0c80bbd50b3ba4550
LautzFromAnotherPlace <davide.lauterio@gmail.com>
2018-06-21 16:53:35 +0200

    deleted junk files

--- SmartyConfig.php
+++ SmartyConfig.php
@@ -1,16 +1,9 @@
 <?php
-define('WHO', 'davide');
 class SmartyConfig
 {
     
     static function configure() : Smarty
     {
-        /*
-        if(WHO == 'marco'){ return SmartyConfig::marcoConf(); }
-        else if(WHO == 'davide')
-        { return SmartyConfig::davideConf(); }
-        else if(WHO == 'giovanni'){ return SmartyConfig::giovConf(); }
-        */
         require('lib/Smarty/Smarty.class.php');
         
         $smarty = new Smarty();
@@ -22,49 +15,5 @@ class SmartyConfig
         
         return $smarty;
     }
-    
-    
-    static function giovConf() : Smarty
-    {
-        require('/opt/lampp/php/Smarty/Smarty.class.php');
-        
-        $smarty = new Smarty();
-        
-        $smarty->setTemplateDir('/opt/lampp/htdocs/deepmusic/smarty/templates');
-        $smarty->setCompileDir('/opt/lampp/htdocs/deepmusic/smarty/templates_c');
-        $smarty->setCacheDir('/opt/lampp/htdocs/deepmusic/smarty/cache');
-        $smarty->setConfigDir('/opt/lampp/htdocs/deepmusic/smarty/configs');
-        
-        return $smarty; 
-    }
-    
-    static function davideConf () : Smarty
-    {
-        require('C:\xampp\php\lib\smarty-3.1.32\libs\Smarty.class.php');
-        
-        $smarty = new Smarty();
-        
-        $smarty->setTemplateDir('C:\xampp\htdocs\musicWebApp\smarty\templates');
-        $smarty->setCompileDir('C:\xampp\htdocs\musicWebApp\smarty\templates_c');
-        $smarty->setCacheDir('C:\xampp\htdocs\musicWebApp\smarty\cache');
-        $smarty->setConfigDir('C:\xampp\htdocs\musicWebApp\smarty\configs');
-        
-        return $smarty;
-    }
-    
-    static function marcoConf () : Smarty
-    {
-        require('C:\xampp\php\lib\Smarty\libs\Smarty.class.php');
-        
-        $smarty = new Smarty();
-        
-        $smarty->setTemplateDir('C:\xampp\htdocs\musicWebApp\smarty\templates');
-        $smarty->setCompileDir('C:\xampp\htdocs\musicWebApp\smarty\templates_c');
-        $smarty->setCacheDir('C:\xampp\htdocs\musicWebApp\smarty\cache');
-        $smarty->setConfigDir('C:\xampp\htdocs\musicWebApp\smarty\configs');
-        
-        return $smarty;
-    }
-    
 }
 
--- deleteSong.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../../inc.php';
-
-if(FPersistantManager::getInstance()->remove('Song', 1))
-    echo "canzone rimossa";
-else
-    echo "canzone NON trovata";
-
-?>
